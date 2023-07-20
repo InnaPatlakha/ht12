73f92d72ba7343c62628fd8eecac6acb17998bf7
Inna Patlakha <i.inna1903@gmail.com>
2023-07-20 23:56:07 +0300

    correction

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 let numOrStr = prompt('input number or string');
-console.log(numOrStr)
 
-switch(numOrStr) {
-  case null:
-    console.log('ви скасували');
+switch(true) {
+  case numOrStr === null:
+    console.log('Cancelled');
     break;
-  case '':
+  case numOrStr.trim() === '':
     console.log('Empty String');
     break;
-  case isNaN( +numOrStr ):
-    console.log(' number is Ba_NaN');
+  case isNaN (+numOrStr):
+    console.log('Not a number');
     break;
   default:
     console.log('OK!');
